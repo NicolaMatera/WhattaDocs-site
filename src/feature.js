db1e9bbe55fb6621e4ac3b2351ee063434934d49
Nicola Matera <nicolamateraposta@gmail.com>
2026-07-30 17:00:18 +0200

    commit fine speriamo

--- src/feature.js
+++ src/feature.js
@@ -1,25 +1,34 @@
-// Selezioniamo tutti gli h3 che hanno un ID che inizia con "f"
-const features = document.querySelectorAll('h3[id^="f"]');
+const features = document.querySelectorAll('li[id^="f"]');
 
 features.forEach(feature => {
     feature.addEventListener('click', () => {
-       
+        
         if (feature.id === 'f1') {
             document.getElementById('img1').classList.remove('hidden');
             document.getElementById('img2').classList.add('hidden');
             document.getElementById('img3').classList.add('hidden');
+            document.getElementById('img4').classList.add('hidden');
         }
-       
+        
         if (feature.id === 'f2') {
             document.getElementById('img2').classList.remove('hidden');
             document.getElementById('img1').classList.add('hidden');
             document.getElementById('img3').classList.add('hidden');
+            document.getElementById('img4').classList.add('hidden');
         }
 
         if (feature.id === 'f3') {
             document.getElementById('img3').classList.remove('hidden');
-            document.getElementById('img2').classList.add('hidden');
             document.getElementById('img1').classList.add('hidden');
+            document.getElementById('img2').classList.add('hidden');
+            document.getElementById('img4').classList.add('hidden');
+        }
+
+        if (feature.id === 'f4') {
+            document.getElementById('img4').classList.remove('hidden');
+            document.getElementById('img1').classList.add('hidden');
+            document.getElementById('img2').classList.add('hidden');
+            document.getElementById('img3').classList.add('hidden');
         }
     });
 });
